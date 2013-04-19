6fc110517f673557af4280d5cba2457a6ae37df0
ptrv <mail@petervasil.net>
2013-04-19 13:08:59 +0200

    Update processing-mode and config

--- ptrv-processing.el
+++ ptrv-processing.el
@@ -26,12 +26,15 @@
 
 (autoload 'processing-mode "processing-mode" "Processing mode" t)
 (add-to-list 'auto-mode-alist '("\\.pde$" . processing-mode))
+(autoload 'processing-snippets-initialize "processing-mode" nil nil nil)
+(eval-after-load 'yasnippet '(processing-snippets-initialize))
 
 (cond
  ((eq system-type 'darwin)
   (setq processing-location "processing-java"))
  ((eq system-type 'gnu/linux)
-  (setq processing-location "~/applications/processing-2.0/processing-java"))
+  ;; (setq processing-location "~/applications/processing-2.0/processing-java")
+  )
  )
 
 
