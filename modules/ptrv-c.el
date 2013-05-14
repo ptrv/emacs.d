cedf6791b41eccfbc1ec36c66001121f90eb1647
ptrv <mail@petervasil.net>
2013-05-14 09:58:28 +0200

    Add commented semantic configuration

--- modules/ptrv-c.el
+++ modules/ptrv-c.el
@@ -78,6 +78,14 @@
 
 ;; (my-ac-config)
 
+;; ;; Semantic
+;; (eval-after-load "semantic"
+;;   '(progn
+;;      (global-semantic-idle-completions-mode t)
+;;      (global-semantic-decoration-mode t)
+;;      (global-semantic-highlight-func-mode t)
+;;      (global-semantic-show-unmatched-syntax-mode t)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'ptrv-c)
