6e17caced783bd7b3d453ff57bf4174f07a77c23
ptrv <mail@petervasil.net>
2013-05-22 17:59:28 +0200

    Improve smart beginning-of-line

--- modules/ptrv-editing.el
+++ modules/ptrv-editing.el
@@ -24,16 +24,32 @@
 
 ;;; Code:
 
-;; Smart home key
-(defun smart-beginning-of-line ()
-  "Move point to first non-whitespace character or beginning-of-line."
-  (interactive "^")
-  (let ((oldpos (point)))
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
     (back-to-indentation)
-    (and (= oldpos (point))
-         (beginning-of-line))))
-(global-set-key (kbd "<home>") 'smart-beginning-of-line)
-(global-set-key (kbd "C-a") 'smart-beginning-of-line)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+;; remap C-a to `smarter-move-beginning-of-line'
+(global-set-key [remap move-beginning-of-line]
+                'smarter-move-beginning-of-line)
 
 ;; Subword mode (consider CamelCase chunks as words)
 ;;(global-subword-mode 1)
