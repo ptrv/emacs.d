c2e75f3b418121a86b3f736f7ac8b71f7b56cfa5
ptrv <mail@petervasil.net>
2013-05-22 09:32:54 +0200

    Initialite smex-save-file before initializeing smex

--- modules/ptrv-ido.el
+++ modules/ptrv-ido.el
@@ -64,12 +64,12 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; smex
+(setq smex-save-file (concat ptrv-tmp-dir "smex-items"))
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-(setq smex-save-file (concat ptrv-tmp-dir "smex-items"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
