b267c71d410f5cce6470d2d4cddd615eb8aeb3d5
ptrv <mail@petervasil.net>
2013-05-25 10:40:58 +0200

    Load erc config only if credentials are set

--- modules/ptrv-erc.el
+++ modules/ptrv-erc.el
@@ -24,43 +24,45 @@
 
 ;;; Code:
 
-(erc-services-mode 1)
-(setq erc-prompt-for-nickserv-password nil)
-(setq erc-nickserv-passwords
-      `((freenode ((,freenode-user . ,freenode-pass)))))
+(when (and (boundp 'freenode-user)
+           (boundp 'freenode-pass))
+  (erc-services-mode 1)
+  (setq erc-prompt-for-nickserv-password nil)
+  (setq erc-nickserv-passwords
+        `((freenode ((,freenode-user . ,freenode-pass)))))
 
-;;IRC
-(erc-autojoin-mode 1)
-(setq erc-autojoin-channels-alist
-      '(("freenode.net" "#emacs")))
+  ;;IRC
+  (erc-autojoin-mode 1)
+  (setq erc-autojoin-channels-alist
+        '(("freenode.net" "#emacs")))
 
-(cond ((string= system-name "alderaan")
-       (setq erc-autojoin-channels-alist
-             (list (append (car erc-autojoin-channels-alist)
-                           '("#supercollider" "#archlinux")))))
-      ((string= system-name "anoth")
-       (setq erc-autojoin-channels-alist
-             (list (append (car erc-autojoin-channels-alist)
-                           '("#supercollider" "#archlinux")))))
-      ;; (t (setq erc-autojoin-channels-alist
-      ;;          '(("freenode.net" "#emacs" "#clojure" "overtone"))))
-      )
+  (cond ((string= system-name "alderaan")
+         (setq erc-autojoin-channels-alist
+               (list (append (car erc-autojoin-channels-alist)
+                             '("#supercollider" "#archlinux")))))
+        ((string= system-name "anoth")
+         (setq erc-autojoin-channels-alist
+               (list (append (car erc-autojoin-channels-alist)
+                             '("#supercollider" "#archlinux")))))
+        ;; (t (setq erc-autojoin-channels-alist
+        ;;          '(("freenode.net" "#emacs" "#clojure" "overtone"))))
+        )
 
 
-(defun erc-connect ()
-  "Start up erc and connect to freedonde"
-  (interactive)
-  (erc :server "irc.freenode.net"
-       :full-name "Peter V."
-       :port 6667
-       :nick freenode-user
-       ))
+  (defun erc-connect ()
+    "Start up erc and connect to freedonde"
+    (interactive)
+    (erc :server "irc.freenode.net"
+         :full-name "Peter V."
+         :port 6667
+         :nick freenode-user
+         ))
 
-(setq erc-keywords `(,freenode-user))
-(erc-match-mode)
+  (setq erc-keywords `(,freenode-user))
+  (erc-match-mode)
 
-(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
-                                "324" "329" "332" "333" "353" "477"))
+  (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
+                                  "324" "329" "332" "333" "353" "477")))
 
 ;;change wrap width when window is resized
 (make-variable-buffer-local 'erc-fill-column)
