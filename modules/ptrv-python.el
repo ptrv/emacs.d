dfd40a9684a7cbae3d682c2ad783e01f2b60da3a
ptrv <mail@petervasil.net>
2013-05-25 16:58:13 +0200

    Surround elpy and python config with after macro

--- modules/ptrv-python.el
+++ modules/ptrv-python.el
@@ -89,106 +89,106 @@
 ;; (add-hook 'python-mode-hook 'load-ropemacs)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(after 'python
+  (exec-path-from-shell-copy-env "PYTHONPATH")
 
-(exec-path-from-shell-copy-env "PYTHONPATH")
+  ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; ;; emacs-jedi
+  ;; (eval-when-compile (require 'jedi nil t))
+  ;; (autoload 'jedi:setup "jedi" nil t)
+  ;; (setq jedi:setup-keys t)
+  ;; (add-hook 'python-mode-hook 'jedi:setup)
+  ;; (setq jedi:complete-on-dot t)
 
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;; emacs-jedi
-;; (eval-when-compile (require 'jedi nil t))
-;; (autoload 'jedi:setup "jedi" nil t)
-;; (setq jedi:setup-keys t)
-;; (add-hook 'python-mode-hook 'jedi:setup)
-;; (setq jedi:complete-on-dot t)
+  ;; (add-hook 'python-mode-hook
+  ;;           (lambda ()
+  ;;             (setq ac-sources
+  ;;                   '(ac-source-dictionary
+  ;;                     ac-source-words-in-buffer
+  ;;                     ac-source-words-in-same-mode-buffers
+  ;;                     ac-source-words-in-all-buffer
+  ;;                     ;; ac-source-yasnippet
+  ;;                     ac-source-semantic))))
 
-;; (add-hook 'python-mode-hook
-;;           (lambda ()
-;;             (setq ac-sources
-;;                   '(ac-source-dictionary
-;;                     ac-source-words-in-buffer
-;;                     ac-source-words-in-same-mode-buffers
-;;                     ac-source-words-in-all-buffer
-;;                     ;; ac-source-yasnippet
-;;                     ac-source-semantic))))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; pytest
+  (autoload 'pytest-all "pytest" nil t)
+  (autoload 'pytest-module "pytest" nil t)
+  (autoload 'pytest-one "pytest" nil t)
+  (autoload 'pytest-directory "pytest" nil t)
+  (setq pytest-global-name "py.test")
+  (add-hook 'python-mode-hook
+            (lambda ()
+              (local-set-key (kbd "C-c C-a") 'pytest-all)
+              (local-set-key (kbd "C-c C-,") 'pytest-module)
+              (local-set-key (kbd "C-c C-.") 'pytest-one)
+              ;;(local-set-key (kbd "C-c C-d") 'pytest-directory)
+              ;; (local-set-key (kbd "C-c t p a") 'pytest-pdb-all)
+              ;; (local-set-key (kbd "C-c t p m") 'pytest-pdb-module)
+              ;; (local-set-key (kbd "C-c t p .") 'pytest-pdb-one)
+              ))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; pytest
-(autoload 'pytest-all "pytest" nil t)
-(autoload 'pytest-module "pytest" nil t)
-(autoload 'pytest-one "pytest" nil t)
-(autoload 'pytest-directory "pytest" nil t)
-(setq pytest-global-name "py.test")
-(add-hook 'python-mode-hook
-          (lambda ()
-            (local-set-key (kbd "C-c C-a") 'pytest-all)
-            (local-set-key (kbd "C-c C-,") 'pytest-module)
-            (local-set-key (kbd "C-c C-.") 'pytest-one)
-            ;;(local-set-key (kbd "C-c C-d") 'pytest-directory)
-            ;; (local-set-key (kbd "C-c t p a") 'pytest-pdb-all)
-            ;; (local-set-key (kbd "C-c t p m") 'pytest-pdb-module)
-            ;; (local-set-key (kbd "C-c t p .") 'pytest-pdb-one)
-            ))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; info
+  (autoload 'info-lookup-add-help "info-look" nil nil)
+  (info-lookup-add-help
+   :mode 'python-mode
+   :regexp "[[:alnum:]_]+"
+   :doc-spec
+   '(("(python)Index" nil "")))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; info
-(autoload 'info-lookup-add-help "info-look" nil nil)
-(info-lookup-add-help
- :mode 'python-mode
- :regexp "[[:alnum:]_]+"
- :doc-spec
- '(("(python)Index" nil "")))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; PythonTidy
+  (defun pytidy-whole-buffer ()
+    (interactive)
+    (let ((a (point)))
+      (shell-command-on-region (point-min) (point-max) "pythontidy" t)
+      (goto-char a)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; PythonTidy
-(defun pytidy-whole-buffer ()
-  (interactive)
-  (let ((a (point)))
-    (shell-command-on-region (point-min) (point-max) "pythontidy" t)
-    (goto-char a)))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; pylookup
+  (eval-when-compile (require 'pylookup))
+  (setq pylookup-dir (concat dotfiles-dir "site-lisp/pylookup"))
+  (setq pylookup-program (concat pylookup-dir "/pylookup.py"))
+  (setq pylookup-db-file (concat pylookup-dir "/pylookup.db"))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; pylookup
-(eval-when-compile (require 'pylookup))
-(setq pylookup-dir (concat dotfiles-dir "site-lisp/pylookup"))
-(setq pylookup-program (concat pylookup-dir "/pylookup.py"))
-(setq pylookup-db-file (concat pylookup-dir "/pylookup.db"))
+  ;; set search option if you want
+  ;; (setq pylookup-search-options '("--insensitive" "0" "--desc" "0"))
 
-;; set search option if you want
-;; (setq pylookup-search-options '("--insensitive" "0" "--desc" "0"))
+  ;; to speedup, just load it on demand
+  (autoload 'pylookup-lookup "pylookup"
+    "Lookup SEARCH-TERM in the Python HTML indexes." t)
 
-;; to speedup, just load it on demand
-(autoload 'pylookup-lookup "pylookup"
-  "Lookup SEARCH-TERM in the Python HTML indexes." t)
+  (autoload 'pylookup-update "pylookup"
+    "Run pylookup-update and create the database at `pylookup-db-file'." t)
 
-(autoload 'pylookup-update "pylookup"
-  "Run pylookup-update and create the database at `pylookup-db-file'." t)
+  (eval-after-load 'python
+    '(progn
+       (define-key python-mode-map (kbd "C-c L") 'pylookup-lookup)))
 
-(eval-after-load 'python
-  '(progn
-     (define-key python-mode-map (kbd "C-c L") 'pylookup-lookup)))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; pylint
+  (autoload 'pylint "pylint")
+  (add-hook 'python-mode-hook 'pylint-add-menu-items)
+  (add-hook 'python-mode-hook 'pylint-add-key-bindings)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; pylint
-(autoload 'pylint "pylint")
-(add-hook 'python-mode-hook 'pylint-add-menu-items)
-(add-hook 'python-mode-hook 'pylint-add-key-bindings)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; hooks
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; hooks
+  ;;(add-hook 'python-mode-hook #'(lambda () (autopair-mode)))
 
-;;(add-hook 'python-mode-hook #'(lambda () (autopair-mode)))
+  ;;(add-hook 'python-mode-hook #'lambda-mode 1)
 
-;;(add-hook 'python-mode-hook #'lambda-mode 1)
-
-;; (add-hook 'python-mode-hook (lambda ()
-;;                               (smart-operator-mode 1)
-;;                               (define-key smart-operator-mode-map "." nil)))
-
+  ;; (add-hook 'python-mode-hook (lambda ()
+  ;;                               (smart-operator-mode 1)
+  ;;                               (define-key smart-operator-mode-map "." nil)))
+  )
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'ptrv-python)
