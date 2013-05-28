94a3e930c91cd376abdf05c27028c49daf832d78
ptrv <mail@petervasil.net>
2013-05-28 17:12:53 +0200

    Clean up linux config

--- modules/ptrv-linux.el
+++ modules/ptrv-linux.el
@@ -24,9 +24,6 @@
 
 ;;; Code:
 
-;; (if (display-graphic-p)
-;;     (progn
-;;       (add-to-list 'default-frame-alist '(font . "Inconsolata-12"))))
 (set-frame-font "Inconsolata-12" nil t)
 (autoload 'pcomplete/apt-get "pcmpl-apt" nil nil)
 
@@ -36,52 +33,22 @@
   )
 (add-hook 'after-make-frame-functions 'setup-frame-hook)
 
-;; Define a function for making desktop notifications
-(autoload 'dbus-call-method "dbus" nil nil)
-(defun dbus-send-desktop-notification (summary body icon timeout)
-  "call notification-daemon method METHOD with ARGS over dbus"
-  (dbus-call-method
-    :session                        ; use the session (not system) bus
-    "org.freedesktop.Notifications" ; service name
-    "/org/freedesktop/Notifications"   ; path name
-    "org.freedesktop.Notifications" "Notify" ; Method
-    "emacs"
-    0 icon summary body
-    '(:array)
-    '(:array (:dict-entry "x-canonical-append" (:variant "allowed")))
-    ':int32 timeout))
+;; erc notification
+(after 'erc
+  (defun my-notify-erc (match-type nickuserhost message)
+    "Notify when a message is received."
+    (unless (posix-string-match "^\\** *Users on #" message)
+      (notify (format "%s in %s"
+                      ;; Username of sender
+                      (car (split-string nickuserhost "!"))
+                      ;; Channel
+                      (or (erc-default-target) "#unknown"))
+              ;; Remove duplicate spaces
+              (replace-regexp-in-string " +" " " message)
+              ;; :icon "/usr/share/notify-osd/icons/gnome/scalable/status/notification-message-im.svg"
+              :timeout -1)))
 
-;; ERC stuff
-
-;; (require 'notifications)
-;; (defun erc-global-notify (match-type nick message)
-;;   "Notify when a message is recieved."
-;;   (unless (posix-string-match "^\\** *Users on #" message)
-;;     (notifications-notify
-;;      :title (format "%s in %s"
-;;                     ;; Username of sender
-;;                     (car (split-string nick "!"))
-;;                     ;; Channel
-;;                     (or (erc-default-target) "#unknown"))
-;;      :body message
-;;      )))
-
-;; (add-hook 'erc-text-matched-hook 'erc-global-notify)
-
-(defun my-notify-erc (match-type nickuserhost message)
-  "Notify when a message is received."
-  (unless (posix-string-match "^\\** *Users on #" message)
-    (notify (format "%s in %s"
-                    ;; Username of sender
-                    (car (split-string nickuserhost "!"))
-                    ;; Channel
-                    (or (erc-default-target) "#unknown"))
-            ;; Remove duplicate spaces
-            (replace-regexp-in-string " +" " " message)
-            ;; :icon "/usr/share/notify-osd/icons/gnome/scalable/status/notification-message-im.svg"
-            :timeout -1)))
-
-(add-hook 'erc-text-matched-hook 'my-notify-erc)
+  (add-hook 'erc-text-matched-hook 'my-notify-erc))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; typeriter-mode
