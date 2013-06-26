b0d1a19536850fc97b0d637694f38b661bb97f64
ptrv <mail@petervasil.net>
2013-06-26 20:22:15 +0200

    Simplify remove-elc-on-save fn

--- init.el
+++ init.el
@@ -668,11 +668,11 @@ file `PATTERNS'."
 
   (defun ptrv/remove-elc-on-save ()
     "If you’re saving an elisp file, likely the .elc is no longer valid."
-    (make-local-variable 'after-save-hook)
     (add-hook 'after-save-hook
               (lambda ()
                 (if (file-exists-p (concat buffer-file-name "c"))
-                    (delete-file (concat buffer-file-name "c")))))))
+                    (delete-file (concat buffer-file-name "c"))))
+              nil :local)))
 
 (ptrv/after ielm
   (dolist (mode ptrv/emacs-lisp-common-modes)
