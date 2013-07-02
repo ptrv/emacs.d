bc94bb6b7c5574bf110ca44968e9e48970db377d
ptrv <mail@petervasil.net>
2013-07-03 00:36:17 +0200

    Simplify hideshow config

--- init.el
+++ init.el
@@ -1986,74 +1986,44 @@ prompt for the command to use."
 (dolist (x '(emacs-lisp lisp java perl sh python))
   (add-hook (intern (concat (symbol-name x) "-mode-hook")) 'hs-minor-mode))
 
-(setq hs-minor-mode-map
-      (let ((map (make-sparse-keymap)))
-        (define-key map (kbd "C-c @ h")   'hs-hide-block)
-        (define-key map (kbd "C-c @ H")   'hs-show-block)
-        (define-key map (kbd "C-c @ s")	  'hs-hide-all)
-        (define-key map (kbd "C-c @ S")	  'hs-show-all)
-        (define-key map (kbd "C-c @ l")   'hs-hide-level)
-        (define-key map (kbd "C-c @ c")   'hs-toggle-hiding)
-        (define-key map [(shift mouse-2)] 'hs-mouse-toggle-hiding)
-        map))
+(ptrv/after hideshow
+  ;; https://github.com/Hawstein/my-emacs/blob/master/_emacs/hs-minor-mode-settings.el
+  (setq hs-isearch-open t)
 
-;; https://github.com/Hawstein/my-emacs/blob/master/_emacs/hs-minor-mode-settings.el
-(setq hs-isearch-open t)
+  (global-set-key (kbd "<f11>") 'hs-toggle-hiding)
+  (global-set-key (kbd "S-<f11>") 'hs-toggle-hiding-all)
 
-;; (global-set-key (kbd "<f11>") 'toggle-fold)
-(global-set-key (kbd "<f11>") 'hs-toggle-hiding)
-(global-set-key (kbd "S-<f11>") 'toggle-fold-all)
+  (defvar hs-hide-all nil
+    "Current state of hideshow for toggling all.")
+  (defun ptrv/hideshow-init ()
+    (ptrv/set-variables-local '(hs-hide-all)))
+  (add-hook 'hs-minor-mode-hook 'ptrv/hideshow-init)
 
-(defvar hs-hide-all nil
-  "Current state of hideshow for toggling all.")
-(defun ptrv/hideshow-init ()
-  (ptrv/set-variables-local '(hs-hide-all)))
-(add-hook 'hs-minor-mode-hook 'ptrv/hideshow-init)
+  (defun hs-toggle-hiding-all ()
+    "Toggle hideshow all."
+    (interactive)
+    (setq hs-hide-all (not hs-hide-all))
+    (if hs-hide-all
+        (hs-hide-all)
+      (hs-show-all)))
 
-(defun hs-toggle-hiding-all ()
-  "Toggle hideshow all."
-  (interactive)
-  (setq hs-hide-all (not hs-hide-all))
-  (if hs-hide-all
-      (hs-hide-all)
-    (hs-show-all)))
-
-(defvar fold-all-fun nil "Function to fold all.")
-(make-variable-buffer-local 'fold-all-fun)
-(defvar fold-fun nil "Function to fold.")
-(make-variable-buffer-local 'fold-fun)
-
-(defun toggle-fold-all ()
-  "Toggle fold all."
-  (interactive)
-  (if fold-all-fun
-      (call-interactively fold-all-fun)
-    (hs-toggle-hiding-all)))
-
-(defun toggle-fold ()
-  "Toggle fold."
-  (interactive)
-  (if fold-fun
-      (call-interactively fold-fun)
-    (hs-toggle-hiding)))
-
-(defadvice goto-line
-  (after
-   expand-after-goto-line
-   activate compile)
-  "hideshow-expand affected block when using goto-line in a
+  (defadvice goto-line
+    (after
+     expand-after-goto-line
+     activate compile)
+    "hideshow-expand affected block when using goto-line in a
 collapsed buffer"
-  (save-excursion
-    (hs-show-block)))
+    (save-excursion
+      (hs-show-block)))
 
-(defadvice goto-line-with-feedback
-  (after
-   expand-after-goto-line-with-feedback
-   activate compile)
-  "hideshow-expand affected block when using goto-line in a
-collapsed buffer"
-  (save-excursion
-    (hs-show-block)))
+  (defadvice goto-line-with-feedback
+    (after
+     expand-after-goto-line-with-feedback
+     activate compile)
+    "hideshow-expand affected block when using
+goto-line-with-feedback in a collapsed buffer"
+    (save-excursion
+      (hs-show-block))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; * autopair
