4862ed84a17ec5f9347ac31607c9f15abdd307b6
ptrv <mail@petervasil.net>
2013-07-06 14:46:01 +0200

    Use default bindings for dired-jump

--- init.el
+++ init.el
@@ -2548,9 +2548,6 @@ goto-line-with-feedback in a collapsed buffer"
 (global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
 (global-set-key (kbd "C-M-z") 'indent-defun)
 
-(global-set-key (kbd "C-x j") 'dired-jump)
-(global-set-key (kbd "C-x 4 j") 'dired-jump-other-window)
-
 ;; Help should search more than just commands
 (define-key 'help-command "a" 'apropos)
 
