d64ea0b13d6bf377adb77f65537cdb78facc4590
ptrv <mail@petervasil.net>
2013-07-13 22:07:36 +0200

    Use built-in fn to make file executable if script

--- init.el
+++ init.el
@@ -1272,17 +1272,7 @@ keymap `ptrv/smartparens-lisp-mode-map'."
 
 ;; make file executabable on save if has shebang
 (add-hook 'after-save-hook
-          #'(lambda ()
-              (and (save-excursion
-                     (save-restriction
-                       (widen)
-                       (goto-char (point-min))
-                       (save-match-data
-                         (looking-at "^#!"))))
-                   (not (file-executable-p buffer-file-name))
-                   (shell-command (concat "chmod u+x " (shell-quote-argument buffer-file-name)))
-                   (message
-                    (concat "Saved as script: " buffer-file-name)))))
+          'executable-make-buffer-file-executable-if-script-p)
 
 ;; fix whitespace-cleanup
 ;; http://stackoverflow.com/a/12958498/464831
