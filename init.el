50f30711b48999abf5dafc5ed27705286016fbe3
ptrv <mail@petervasil.net>
2013-08-28 11:56:55 +0200

    Add new line in git-commit-mode prompt

--- init.el
+++ init.el
@@ -1084,7 +1084,17 @@ keymap `ptrv/smartparens-lisp-mode-map'."
   (add-hook 'git-commit-mode-hook
             (lambda ()
               (set-fill-column 72)
-              (auto-fill-mode 1))))
+              (auto-fill-mode 1)))
+
+  ;; Add an extra newline to separate commit message from git commentary
+  (defun magit-commit-mode-init ()
+    (when (looking-at "\n")
+      (open-line 1)))
+  (add-hook 'git-commit-mode-hook 'magit-commit-mode-init)
+  ;; ;; close popup when commiting
+  ;; (defadvice git-commit-commit (after delete-window activate)
+  ;;   (delete-window))
+  )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; * git-messanger
