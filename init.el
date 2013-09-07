7f28dfa1c6a778c94d0137b6ce61269eb8f52117
ptrv <mail@petervasil.net>
2013-09-07 11:25:18 +0200

    Disable pretty symbols for clojure-mode

--- init.el
+++ init.el
@@ -886,21 +886,21 @@ keymap `ptrv/smartparens-lisp-mode-map'."
 
   (ptrv/smartparens-setup-lisp-modes '(clojure-mode))
 
-  (font-lock-add-keywords
-   'clojure-mode `(("(\\(fn\\)[\[[:space:]]"
-                    (0 (progn (compose-region (match-beginning 1)
-                                              (match-end 1) "λ")
-                              nil)))))
-  (font-lock-add-keywords
-   'clojure-mode `(("\\(#\\)("
-                    (0 (progn (compose-region (match-beginning 1)
-                                              (match-end 1) "ƒ")
-                              nil)))))
-  (font-lock-add-keywords
-   'clojure-mode `(("\\(#\\){"
-                    (0 (progn (compose-region (match-beginning 1)
-                                              (match-end 1) "∈")
-                              nil)))))
+  ;; (font-lock-add-keywords
+  ;;  'clojure-mode `(("(\\(fn\\)[\[[:space:]]"
+  ;;                   (0 (progn (compose-region (match-beginning 1)
+  ;;                                             (match-end 1) "λ")
+  ;;                             nil)))))
+  ;; (font-lock-add-keywords
+  ;;  'clojure-mode `(("\\(#\\)("
+  ;;                   (0 (progn (compose-region (match-beginning 1)
+  ;;                                             (match-end 1) "ƒ")
+  ;;                             nil)))))
+  ;; (font-lock-add-keywords
+  ;;  'clojure-mode `(("\\(#\\){"
+  ;;                   (0 (progn (compose-region (match-beginning 1)
+  ;;                                             (match-end 1) "∈")
+  ;;                             nil)))))
   (add-hook 'clojure-mode-hook
             (lambda ()
               (setq buffer-save-without-query t)))
