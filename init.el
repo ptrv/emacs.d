032aebac3088d6fc4194ed506962516d0b809ad0
ptrv <mail@petervasil.net>
2013-09-30 11:31:25 +0200

    Load PATH with own function
    When using .zshenv file for setting ENV variables exec-path-from-shell
    does not load PATH properly.

--- init.el
+++ init.el
@@ -328,7 +328,19 @@ file `PATTERNS'."
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; * PATH
-(exec-path-from-shell-initialize)
+;; (exec-path-from-shell-initialize)
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+
+This is particularly useful under Mac OSX, where GUI apps are not
+started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string
+                          "[ \t\n]*$" ""
+                          (shell-command-to-string "$SHELL -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(set-exec-path-from-shell-PATH)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; * builtins
