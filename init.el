9160def0e3f65880f66ebda5d9e1e6f8aa18e037
ptrv <mail@petervasil.net>
2014-03-08 15:53:31 +0100

    Remove x11 config

--- init.el
+++ init.el
@@ -2491,27 +2491,6 @@ collapsed buffer"
   (require 'smartparens-ruby))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; * x11
-(when *is-x11*
-  ;; Maximise the Emacs window
-  ;; (defun toggle-fullscreen ()
-  ;;   (interactive)
-  ;;   (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-  ;;                          '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
-  ;;   (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-  ;;                          '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
-
-  (cond ((and (<= (x-display-pixel-width) 1280)
-              (<= (x-display-pixel-height) 800))
-         ;; (toggle-fullscreen)
-         )
-        ((and (eq (x-display-pixel-width) 1680)
-              (eq (x-display-pixel-height) 1050))
-         ;; (set-frame-size (selected-frame) 110 60)
-         (set-frame-size (selected-frame) 130 55)
-         (set-frame-position (selected-frame) 500 30))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; * multiple-cursors
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
