17beb4e1603e3772e0248844697d5b6a7d174b8f
ptrv <mail@petervasil.net>
2014-08-03 19:37:21 +0200

    Colorize compilation buffer properly on mac

--- init.el
+++ init.el
@@ -2151,7 +2151,21 @@ collapsed buffer"
   (ptrv/after locate
     (let ((mdfind (executable-find "mdfind")))
       (when mdfind
-        (setq locate-command mdfind)))))
+        (setq locate-command mdfind))))
+
+  (defun ptrv/colorize-compilation-buffer ()
+    "Taken from `https://github.com/lunaryorn/.emacs.d'"
+    (interactive)
+    (when (eq major-mode 'compilation-mode)
+      (let ((inhibit-read-only t))
+        (ansi-color-apply-on-region (point-min) (point-max)))))
+
+  ;; Compilation from Emacs
+  (ptrv/after compile
+    ;; Colorize output of Compilation Mode, see
+    ;; http://stackoverflow.com/a/3072831/355252
+    (require 'ansi-color)
+    (add-hook 'compilation-filter-hook 'ptrv/colorize-compilation-buffer)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; * linux
