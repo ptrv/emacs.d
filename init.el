1b95a6272c4ee8e7e8010ee5913ec90fc63294dd
ptrv <mail@petervasil.net>
2014-10-10 10:34:01 +0200

    Remove smartparens custom key bindings and minor refinements

--- init.el
+++ init.el
@@ -664,36 +664,36 @@ Source: `https://github.com/lunaryorn/.emacs.d'"
 (require 'smartparens-config)
 
 (ptrv/after smartparens
-  (let ((map smartparens-mode-map))
-    ;; Movement and navigation
-    (define-key map (kbd "C-M-f") #'sp-forward-sexp)
-    (define-key map (kbd "C-M-b") #'sp-backward-sexp)
-    (define-key map (kbd "C-M-u") #'sp-backward-up-sexp)
-    (define-key map (kbd "C-M-d") #'sp-down-sexp)
-    (define-key map (kbd "C-M-p") #'sp-backward-down-sexp)
-    (define-key map (kbd "C-M-n") #'sp-up-sexp)
-    ;; Deleting and killing
-    (define-key map (kbd "C-M-k") #'sp-kill-sexp)
-    (define-key map (kbd "C-M-w") #'sp-copy-sexp)
-    ;; Depth changing
-    (define-key map (kbd "M-s") #'sp-splice-sexp)
-    (define-key map (kbd "M-<up>") #'sp-splice-sexp-killing-backward)
-    (define-key map (kbd "M-<down>") #'sp-splice-sexp-killing-forward)
-    (define-key map (kbd "M-r") #'sp-splice-sexp-killing-around)
-    (define-key map (kbd "M-?") #'sp-convolute-sexp)
-    ;; Barfage & Slurpage
-    (define-key map (kbd "C-)")  #'sp-forward-slurp-sexp)
-    (define-key map (kbd "C-<right>") #'sp-forward-slurp-sexp)
-    (define-key map (kbd "C-}")  #'sp-forward-barf-sexp)
-    (define-key map (kbd "C-<left>") #'sp-forward-barf-sexp)
-    (define-key map (kbd "C-(")  #'sp-backward-slurp-sexp)
-    (define-key map (kbd "C-M-<left>") #'sp-backward-slurp-sexp)
-    (define-key map (kbd "C-{")  #'sp-backward-barf-sexp)
-    (define-key map (kbd "C-M-<right>") #'sp-backward-barf-sexp)
-    ;; Miscellaneous commands
-    (define-key map (kbd "M-S") #'sp-split-sexp)
-    (define-key map (kbd "M-J") #'sp-join-sexp)
-    (define-key map (kbd "C-M-t") #'sp-transpose-sexp))
+  ;; (let ((map smartparens-mode-map))
+  ;;   ;; Movement and navigation
+  ;;   (define-key map (kbd "C-M-f") #'sp-forward-sexp)
+  ;;   (define-key map (kbd "C-M-b") #'sp-backward-sexp)
+  ;;   (define-key map (kbd "C-M-u") #'sp-backward-up-sexp)
+  ;;   (define-key map (kbd "C-M-d") #'sp-down-sexp)
+  ;;   (define-key map (kbd "C-M-p") #'sp-backward-down-sexp)
+  ;;   (define-key map (kbd "C-M-n") #'sp-up-sexp)
+  ;;   ;; Deleting and killing
+  ;;   (define-key map (kbd "C-M-k") #'sp-kill-sexp)
+  ;;   (define-key map (kbd "C-M-w") #'sp-copy-sexp)
+  ;;   ;; Depth changing
+  ;;   (define-key map (kbd "M-s") #'sp-splice-sexp)
+  ;;   (define-key map (kbd "M-<up>") #'sp-splice-sexp-killing-backward)
+  ;;   (define-key map (kbd "M-<down>") #'sp-splice-sexp-killing-forward)
+  ;;   (define-key map (kbd "M-r") #'sp-splice-sexp-killing-around)
+  ;;   (define-key map (kbd "M-?") #'sp-convolute-sexp)
+  ;;   ;; Barfage & Slurpage
+  ;;   (define-key map (kbd "C-)")  #'sp-forward-slurp-sexp)
+  ;;   (define-key map (kbd "C-<right>") #'sp-forward-slurp-sexp)
+  ;;   (define-key map (kbd "C-}")  #'sp-forward-barf-sexp)
+  ;;   (define-key map (kbd "C-<left>") #'sp-forward-barf-sexp)
+  ;;   (define-key map (kbd "C-(")  #'sp-backward-slurp-sexp)
+  ;;   (define-key map (kbd "C-M-<left>") #'sp-backward-slurp-sexp)
+  ;;   (define-key map (kbd "C-{")  #'sp-backward-barf-sexp)
+  ;;   (define-key map (kbd "C-M-<right>") #'sp-backward-barf-sexp)
+  ;;   ;; Miscellaneous commands
+  ;;   (define-key map (kbd "M-S") #'sp-split-sexp)
+  ;;   (define-key map (kbd "M-J") #'sp-join-sexp)
+  ;;   (define-key map (kbd "C-M-t") #'sp-transpose-sexp))
 
   ;; Some additional bindings for strict mode
   (let ((map smartparens-strict-mode-map))
@@ -715,7 +715,10 @@ keymap `ptrv/smartparens-lisp-mode-map'."
   (sp-local-pair modes "(" nil :bind "M-(")
   (dolist (mode modes)
     (let ((hook (intern (format "%s-hook" (symbol-name mode)))))
-      (add-hook hook 'smartparens-strict-mode))))
+      (add-hook hook (lambda ()
+                       (dolist (it sp-paredit-bindings)
+                         (local-set-key (read-kbd-macro (car it)) (cdr it)))
+                       (smartparens-strict-mode +1))))))
 
 ;;"Enable `smartparens-mode' in the minibuffer, during
 ;;`eval-expression'."
@@ -725,9 +728,9 @@ keymap `ptrv/smartparens-lisp-mode-map'."
 (if (boundp 'eval-expression-minibuffer-setup-hook)
     (add-hook 'eval-expression-minibuffer-setup-hook 'turn-on-sp)
   (add-hook 'minibuffer-setup-hook
-            #'(lambda ()
-                (when (eq this-command 'eval-expression)
-                  (turn-on-sp)))))
+            (lambda ()
+              (when (eq this-command 'eval-expression)
+                (turn-on-sp)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; * lisp
