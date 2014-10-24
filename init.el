550419db9c70f4254f58a995199982d454a3f6cc
ptrv <mail@petervasil.net>
2014-10-24 17:38:56 +0200

    Add emacs lisp header

--- init.el
+++ init.el
@@ -1,4 +1,33 @@
-;;; init.el --- ptrv init file
+;;; init.el --- Emacs configuration of Peter Vasil
+;;
+;; Copyright (c) 2013-2014 Peter Vasil <mail@petervasil.net>
+;;
+;; Author: Peter Vasil <mail@petervasil.net>
+;; URL: https://gihub.com/ptrv/emacs.d
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation; either version 3 of the License, or (at your option) any later
+;; version.
+
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+;; details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; GNU Emacs; see the file COPYING.  If not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+;; USA.
+
+;;; Commentary:
+
+;; Emacs configuration of Peter Vasil.
+
+;;; Code:
 
 (when (version-list-< (version-to-list emacs-version) '(24 4))
   (error "This configuration needs Emacs 24.4, but this is %s!" emacs-version))
@@ -3017,10 +3046,10 @@ This checks in turn:
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; init.el ends here
-
 ;; Local Variables:
 ;; eval: (orgstruct-mode 1)
 ;; orgstruct-heading-prefix-regexp: ";;;; "
 ;; byte-compile-warnings: (not cl-functions)
 ;; End:
+
+;;; init.el ends here
