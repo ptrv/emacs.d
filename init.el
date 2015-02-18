00a6a42fccb2c38ac40b9f5ef66885fd78d63916
ptrv <mail@petervasil.net>
2015-02-18 11:52:14 +0100

    Minor dired config refinements

--- init.el
+++ init.el
@@ -1714,7 +1714,16 @@ If ARG is not nil, create package in current directory"
   (progn
     (require 'dired-x)
     (setq dired-auto-revert-buffer t
-          dired-listing-switches "-ahl")))
+          dired-listing-switches "-alhF")
+
+    (when (or (memq system-type '(gnu gnu/linux))
+              (string= (file-name-nondirectory insert-directory-program) "gls"))
+      ;; If we are on a GNU system or have GNU ls, add some more `ls' switches:
+      ;; `--group-directories-first' lists directories before files, and `-v'
+      ;; sorts numbers in file names naturally, i.e. "image1" goes before
+      ;; "image02"
+      (setq dired-listing-switches
+            (concat dired-listing-switches " --group-directories-first -v")))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; * projectile
