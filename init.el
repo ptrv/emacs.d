b52c6e5fecb0759caaafcae06206f1b0376379b3
ptrv <mail@petervasil.net>
2015-09-29 11:52:39 +0200

    Advice erc-nickserv-identify in order to be able to pass a function as password

--- init.el
+++ init.el
@@ -1760,15 +1760,28 @@ If ARG is not nil, create package in current directory"
     (use-package erc-services
       :config
       (progn
+        (defun ptrv/get-nickserv-password (network)
+          (let ((creds (netrc-credentials network)))
+            (cadr creds)))
+        (defun ptrv/erc-nickserv-identify (orig-func &rest args)
+          (let* ((arg (car args))
+                 (password (if (functionp arg)
+                               (funcall arg)
+                             arg)))
+            (apply orig-func (list password))))
+        (advice-add 'erc-nickserv-identify :around
+                    #'ptrv/erc-nickserv-identify)
         (erc-services-mode)
         (setq erc-prompt-for-nickserv-password nil)
         (let ((freenode-credentials (netrc-credentials "freenode"))
               (oftc-credentials (netrc-credentials "oftc")))
           (setq erc-nickserv-passwords
                 `((freenode (,(cons (car freenode-credentials)
-                                    (cadr freenode-credentials))))
+                                    (apply-partially 'ptrv/get-nickserv-password
+                                                     "freenode"))))
                   (oftc (,(cons (car oftc-credentials)
-                                (cadr oftc-credentials)))))))))
+                                (apply-partially 'ptrv/get-nickserv-password
+                                                 "oftc")))))))))
 
     (use-package erc-track
       :config
