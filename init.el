45fcbbcc8a2bf778a9b1a75e602a0ee7687dbbd7
ptrv <mail@petervasil.net>
2016-01-21 17:25:57 +0100

    Remove manually added c++11 font-lock keywords

--- init.el
+++ init.el
@@ -2155,26 +2155,7 @@ This checks in turn:
       (add-hook 'c++-mode-hook #'cscope-minor-mode))
 
     (use-package clang-format
-      :ensure t)
-
-    ;; C++11 keywords
-    (font-lock-add-keywords
-     'c++-mode
-     `(;; complete some fundamental keywords
-       ("\\<\\(void\\|unsigned\\|signed\\|char\\|short\\|bool\\|int\\|long\\|float\\|double\\)\\>" . font-lock-keyword-face)
-       ;; add the new C++11 keywords
-       ("\\<\\(alignof\\|alignas\\|constexpr\\|decltype\\|noexcept\\|nullptr\\|static_assert\\|thread_local\\|override\\|final\\)\\>" . font-lock-keyword-face)
-       ("\\<\\(char[0-9]+_t\\)\\>" . font-lock-keyword-face)
-       ;; PREPROCESSOR_CONSTANT
-       ("\\<[A-Z]+[A-Z_]+\\>" . font-lock-constant-face)
-       ;; hexadecimal numbers
-       ("\\<0[xX][0-9A-Fa-f]+\\>" . font-lock-constant-face)
-       ;; integer/float/scientific numbers
-       ("\\<[\\-+]*[0-9]*\\.?[0-9]+\\([ulUL]+\\|[eE][\\-+]?[0-9]+\\)?\\>" . font-lock-constant-face)
-       ;; user-types (customize!)
-       ("\\<[A-Za-z_]+[A-Za-z_0-9]*_\\(t\\|type\\|ptr\\)\\>" . font-lock-type-face)
-       ("\\<\\(xstring\\|xchar\\)\\>" . font-lock-type-face)
-       ) :append)))
+      :ensure t)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; * gud
