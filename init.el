a5d81af2d85f9a7d43e6e23235a277c963be89f7
ptrv <mail@petervasil.net>
2016-04-10 19:05:50 +0200

    Add dired-rsync function

--- init.el
+++ init.el
@@ -1942,7 +1942,36 @@ With a prefix argument P, isearch for the symbol at point."
     ;; sorts numbers in file names naturally, i.e. "image1" goes before
     ;; "image02"
     (setq dired-listing-switches
-          (concat dired-listing-switches " --group-directories-first -v"))))
+          (concat dired-listing-switches " --group-directories-first -v")))
+  (defun ptrv/dired-rsync (dest)
+    (interactive
+     (list
+      (expand-file-name
+       (read-file-name
+        "Rsync to:"
+        (dired-dwim-target-directory)))))
+    ;; store all selected files into "files" list
+    (let ((files (dired-get-marked-files
+                  nil current-prefix-arg))
+          ;; the rsync command
+          (ptrv/rsync-command
+           "rsync -arvz --progress "))
+      ;; add all selected file names as arguments
+      ;; to the rsync command
+      (dolist (file files)
+        (setq ptrv/rsync-command
+              (concat ptrv/rsync-command
+                      (shell-quote-argument file)
+                      " ")))
+      ;; append the destination
+      (setq ptrv/rsync-command
+            (concat ptrv/rsync-command
+                    (shell-quote-argument dest)))
+      ;; run the async shell command
+      (async-shell-command ptrv/rsync-command "*rsync*")
+      ;; finally, switch to that window
+      (other-window 1)))
+  (bind-key "Y" #'ptrv/dired-rsync dired-mode-map))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; * projectile
