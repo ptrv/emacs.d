b048e3771c7aab13080f2b56df144bf3536be793
ptrv <mail@petervasil.net>
2016-05-30 00:58:11 +0200

    Add magit-gitflow package

--- init.el
+++ init.el
@@ -1194,7 +1194,11 @@ This checks in turn:
     (ptrv/magit-set-repo-dirs-from-projectile))
 
   (add-hook 'projectile-switch-project-hook
-            #'ptrv/magit-set-repo-dirs-from-projectile))
+            #'ptrv/magit-set-repo-dirs-from-projectile)
+
+  (use-package magit-gitflow
+    :ensure t
+    :init (add-hook 'magit-mode-hook #'turn-on-magit-gitflow)))
 
 (use-package git-commit
   :ensure t
