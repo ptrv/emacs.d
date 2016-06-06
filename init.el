8f83371eb17617601d2599e6202a1f4485f2b0d5
ptrv <mail@petervasil.net>
2016-06-06 23:54:57 +0200

    Add hydra for smartparens

--- init.el
+++ init.el
@@ -909,30 +909,73 @@ Something like: `python -m certifi'."
 ;;;; * smartparens
 (use-package smartparens
   :ensure t
+  :bind (("C-c k" . ptrv/smartparens/body)
+         :map smartparens-strict-mode
+         ("M-q" . sp-indent-defun))
   :init
-  (require 'smartparens-config)
+  (defhydra ptrv/smartparens (:hint nil)
+    "
+Sexps (quit with _q_)
+^Nav^            ^Barf/Slurp^                 ^Depth^
+^---^------------^----------^-----------------^-----^-----------------
+_f_: forward     _→_:          slurp forward   _R_: splice
+_b_: backward    _←_:          barf forward    _r_: raise
+_u_: backward ↑  _C-<right>_:  slurp backward  _↑_: raise backward
+_d_: forward ↓   _C-<left>_:   barf backward   _↓_: raise forward
+_p_: backward ↓
+_n_: forward ↑
+^Kill^           ^Misc^                       ^Wrap^
+^----^-----------^----^-----------------------^----^------------------
+_w_: copy        _j_: join                    _(_: wrap with ( )
+_k_: kill        _s_: split                   _{_: wrap with { }
+^^               _t_: transpose               _'_: wrap with ' '
+^^               _c_: convolute               _\"_: wrap with \" \"
+^^               _i_: indent defun"
+    ("q" nil)
+    ;; Wrapping
+    ("(" (lambda (_) (interactive "P") (sp-wrap-with-pair "(")))
+    ("{" (lambda (_) (interactive "P") (sp-wrap-with-pair "{")))
+    ("'" (lambda (_) (interactive "P") (sp-wrap-with-pair "'")))
+    ("\"" (lambda (_) (interactive "P") (sp-wrap-with-pair "\"")))
+    ;; Navigation
+    ("f" sp-forward-sexp )
+    ("b" sp-backward-sexp)
+    ("u" sp-backward-up-sexp)
+    ("d" sp-down-sexp)
+    ("p" sp-backward-down-sexp)
+    ("n" sp-up-sexp)
+    ;; Kill/copy
+    ("w" sp-copy-sexp)
+    ("k" sp-kill-sexp)
+    ;; Misc
+    ("t" sp-transpose-sexp)
+    ("j" sp-join-sexp)
+    ("s" sp-split-sexp)
+    ("c" sp-convolute-sexp)
+    ("i" sp-indent-defun)
+    ;; Depth changing
+    ("R" sp-splice-sexp)
+    ("r" sp-splice-sexp-killing-around)
+    ("<up>" sp-splice-sexp-killing-backward)
+    ("<down>" sp-splice-sexp-killing-forward)
+    ;; Barfing/slurping
+    ("<right>" sp-forward-slurp-sexp)
+    ("<left>" sp-forward-barf-sexp)
+    ("C-<left>" sp-backward-barf-sexp)
+    ("C-<right>" sp-backward-slurp-sexp))
+
   (smartparens-global-mode)
   (show-smartparens-global-mode)
+
+  (dolist (hook '(inferior-emacs-lisp-mode-hook
+                  emacs-lisp-mode-hook
+                  eval-expression-minibuffer-setup-hook))
+    (add-hook hook #'smartparens-strict-mode))
   :config
+  (require 'smartparens-config)
+
   (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
-
-  (dolist (it sp-paredit-bindings)
-    (bind-key (car it) (cdr it) smartparens-strict-mode-map))
-
-  (bind-key "M-q" #'sp-indent-defun smartparens-strict-mode-map)
-  (bind-key "C-j" #'sp-newline smartparens-strict-mode-map)
-  (bind-key "M-?" #'sp-convolute-sexp smartparens-strict-mode-map)
-
-  (sp-local-pair sp--lisp-modes "(" nil :bind "M-(")
-
-  (dolist (mode sp--lisp-modes)
-    (unless (eq mode 'eshell-mode)
-      (let ((hook (intern (format "%s-hook" (symbol-name mode)))))
-        (add-hook hook 'smartparens-strict-mode))))
-
-  (ptrv/hook-into-modes #'smartparens-strict-mode
-    '(eval-expression-minibuffer-setup-hook ielm-mode-hook))
-
+  (sp-local-pair sp-lisp-modes "(" nil :bind "M-(")
   (sp-local-pair '(c++-mode) "{" nil :post-handlers
                  '(((lambda (&rest _ignored)
                       (ptrv/smart-open-line-above)) "RET"))))
