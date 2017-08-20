457718fa9952b49fddd3a7d1d912b354e9733c9c
ptrv <mail@petervasil.net>
2017-08-20 16:21:10 +0200

    Add hydra for smerge

--- init.el
+++ init.el
@@ -1276,6 +1276,37 @@ This checks in turn:
   :ensure t
   :bind ("C-c g T" . git-timemachine-toggle))
 
+(use-package smerge-mode
+  :bind (:map smerge-mode-map
+              ("C-c m m" . hydra-smerge/body))
+  :config
+  (defhydra hydra-smerge
+    (:color red :hint nil)
+    "
+^Move^ ^Keep^ ^Diff^ ^Pair^
+------------------------------------------------------
+_n_ext _b_ase _R_efine _<_: base-mine
+_p_rev _m_ine _E_diff _=_: mine-other
+^ ^ _o_ther _C_ombine _>_: base-other
+^ ^ _a_ll _r_esolve
+_q_uit _RET_: current
+"
+    ("RET" smerge-keep-current)
+    ("C" smerge-combine-with-next)
+    ("E" smerge-ediff)
+    ("R" smerge-refine)
+    ("a" smerge-keep-all)
+    ("b" smerge-keep-base)
+    ("m" smerge-keep-mine)
+    ("n" smerge-next)
+    ("o" smerge-keep-other)
+    ("p" smerge-prev)
+    ("r" smerge-resolve)
+    ("<" smerge-diff-base-mine)
+    ("=" smerge-diff-mine-other)
+    (">" smerge-diff-base-other)
+    ("q" nil :color blue)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; * vc
 (use-package vc-hooks
