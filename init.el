ee22b76e5b2d27db5a0a61a6da475660c63facc1
ptrv <mail@petervasil.net>
2017-08-28 12:08:41 +0200

    Diminish helm-scope-mode

--- init.el
+++ init.el
@@ -2609,7 +2609,8 @@ With a prefix argument P, isearch for the symbol at point."
           ("C-c , =" . helm-cscope-find-assignments-to-this-symbol))
     :init
     (dolist (hook '(c++-mode-hook c-mode-hook))
-      (add-hook hook #'helm-cscope-mode))))
+      (add-hook hook #'helm-cscope-mode))
+    :diminish helm-cscope-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; * gud
